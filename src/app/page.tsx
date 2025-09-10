e300b07ed655590c7ec8e9a15c306eebf62f3fa4
ShamirAli55 <shamirali9779@gmail.com>
2025-09-10 15:40:32 +0500

    Navbar implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Page(){
+import Image from "next/image";
+import Hero from "../../public/hero.png";
+export default function Page() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex justify-between">
+      <div className="h-full w-1/2 ">
+        <h1 className="text-7xl font-bold w-[90%]">Better design for your digital products.</h1>
+        <p className="text-xl py-8 tracking-wide w-[85%]">Turning your ideas into Reality. We bring together the teams from the global tech industry.</p>
+        <button className="bg-green-400 text-white py-4 px-6 rounded-lg hover:cursor-pointer hover:bg-green-600">See our works</button>
+      </div>
+      <div className="h-full w-1/2 px-22">
+        <Image src={Hero} alt="hero" className="w-96 object-cover" />
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,11 @@ import { links } from "@/constants"
 export default function Navbar() {
     return (
         <div className="w-full flex items-center justify-between py-8">
-            <Link href={"/"}>Lamamia</Link>
+            <Link href={"/"} className="hover:scale-105 transition-all duration-300">Lamamia</Link>
             <div className="flex items-center gap-x-3">
                 {links.map((link) => (
-                    <Link key={link.id} href={link.url}>{link.name}</Link>
+                    <Link key={link.id} href={link.url}
+                        className="hover:scale-105 transition-all duration-300">{link.name}</Link>
                 ))}
             </div>
         </div>
