c20c1a8761a305b8dc9000f2e6db43e78a2026bb
ShamirAli55 <shamirali9779@gmail.com>
2025-09-10 17:54:16 +0500

    minor fixes

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -6,8 +6,8 @@ export default function DarkMode() {
     const  {themetoggle,mode} = useContext(ThemeContext);
     return (
         <div className="flex items-center border rounded-full justify-between gap-x-1 text-xs  relative overflow-hidden cursor-pointer" onClick={themetoggle}>
-            <div className="p-0.5">🔆</div>
-            <div className="p-0.5">🌙</div>
+            <div className="p-0.5 pointer-events-none">🔆</div>
+            <div className="p-0.5 pointer-events-none">🌙</div>
             <div className={`absolute w-1/2 h-full bg-green-400 rounded-[100px] ${mode === "light" ? "left-0" : "right-0"}`}></div>
         </div>
     )
